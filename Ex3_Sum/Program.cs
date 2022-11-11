4d496601fe53840b44b9fab247a1c2ebfdebd703
Vladimir <vlborshh@yandex.ru>
2022-11-11 11:46:08 +0300

    Ex3_Sum

---
+++ Ex3_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое ЦЕЛОЕ число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе ЦЕЛОЕ число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Сумма введеных чисел: {numberA + numberB}");
+
